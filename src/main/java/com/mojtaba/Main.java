861f3d63a9f6b0c5a6905b175ce4e09357907b8b
Mojtaba Haddadi <Mojtaba.Haddadi@Snapp.cab>
2021-03-25 01:42:51 +0430

    Modify main method to work with new generated files

--- src/main/java/com/mojtaba/Main.java
+++ src/main/java/com/mojtaba/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args)
     {
-        DataBase dataBase = com.mojtaba.DataBase_singleton.getInstance();
-        Network network = com.mojtaba.Network_singleton.getInstance();
+        DataBase dataBase = com.mojtaba.AllSingletons.getDataBaseInstance();
+        Network network = com.mojtaba.AllSingletons.getNetworkInstance();
 
         System.out.println(dataBase.getSampleData());
         System.out.println(network.getSampleData());
