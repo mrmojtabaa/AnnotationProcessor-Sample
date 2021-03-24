f756d7b228f9afcfd50382b5543869bf7567cef3
Mojtaba Haddadi <Mojtaba.Haddadi@Snapp.cab>
2021-03-24 04:33:15 +0430

    Create base javaProject

---
+++ src/main/java/example_myprocessor/SimpleAnnotation.java
@@ -0,0 +1,10 @@
+package example_myprocessor;
+
+public class SimpleAnnotation {
+
+    public static void main(String[] args)
+    {
+        String greetings = "Hello World";
+        System.out.println(greetings);
+    }
+}
