f36640d3796418c7611a193634c4144c149914d5
Mojtaba Haddadi <Mojtaba.Haddadi@Snapp.cab>
2021-03-24 21:13:47 +0430

    Add test classes annotated with @MySingleton annotation

---
+++ src/main/java/com/mojtaba/DataBase.java
@@ -0,0 +1,11 @@
+package com.mojtaba;
+
+@MySingleton
+public class DataBase {
+
+    public String getSampleData()
+    {
+        return "Returned data from database";
+    }
+
+}
---
+++ src/main/java/com/mojtaba/Network.java
@@ -0,0 +1,11 @@
+package com.mojtaba;
+
+@MySingleton
+public class Network {
+
+    public String getSampleData()
+    {
+        return "Returned data from network";
+    }
+
+}
